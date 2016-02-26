3d7956e7ab96ea68f8ab2df0fda11ba784cc9e7f
Tan Zhang <tanzhang0504@users.noreply.github.com>
2016-02-25 19:26:08 -0600

    Update throughput_gps.py
    Add printing time.

--- experiment/throughput_gps.py
+++ experiment/throughput_gps.py
@@ -45,54 +45,43 @@ def parse_throughput_loss_per_line(line):
     if m2:
         lossRate = float(m2.group(1))
         lossRate = lossRate/100.
-
-
     return throughput, lossRate
 
-
-
-
-outputFile = None
-device = None
-python_script = None
-
-if len(sys.argv) == 4:
-    outputFile = sys.argv[3]
-    device = sys.argv[2]
+if __name__ == '__main__':
+    outputFile = None
+    device = None
+    python_script = None
     python_script = sys.arg[1]
-elif len(sys.argv) == 3:
     device = sys.argv[2]
-    python_script = sys.arg[1]
-else:
-    print "This script takes a device and an optional file as arguments"
-    sys.exit(1)
-
-gps_info = gps_parser.GPSInfo()
-gps_parse = gps_parser.GPSParser()
-thread = threading.Thread(target=gps_parser.GPSParser.read_gps, args=(gps_parse,gps_info, device))
-thread.start()
-
-time.sleep(2)
-
-outf = open(outputFile, 'w') if outputFile else sys.stdout
-
-cmd = "python " + python_script
-baseratePipe = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
-mode = fcntl.fcntl(baseratePipe.stdout.fileno(), fcntl.F_GETFL) | os.O_NONBLOCK
-fcntl.fcntl(baseratePipe.stdout.fileno(), fcntl.F_SETFL, mode) # file lock
-while baseratePipe.poll() == None:
-    readx = select.select([baseratePipe.stdout.fileno()], [], [])[0]
-    if readx:
-        while True:
-            try:
-                line = baseratePipe.stdout.readline()
-            except IOError:
-                continue
-            break
-        throughput, lossRate = parse_throughput_loss_per_line(line)
-        lat, lon, speed, time = getGpsData(gps_info)
-        outf.write(("%.6f" % lat) + " " + \
-                ("%.6f" % lon) + " " + ("%.3f" % speed) + " " + str(throughput) + " " + str(lossRate) + "\n")
-
-if outputFile != None:
-    close(outf)
+    if len(sys.argv) != 3 and len(sys.argv) != 4:
+        print "Usage: %s iperf_script gps_dev [log_file]" % sys.argv[0]
+        sys.exit(0)
+    else if len(sys.argv) == 4:
+        outputFile = sys.argv[3]
+    
+    gps_info = gps_parser.GPSInfo()
+    gps_parse = gps_parser.GPSParser()
+    thread = threading.Thread(target=gps_parser.GPSParser.read_gps, args=(gps_parse, gps_info, device, true))
+    thread.start()
+    time.sleep(2)
+    outf = open(outputFile, 'w') if outputFile else sys.stdout
+    cmd = "python " + python_script
+    baseratePipe = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
+    mode = fcntl.fcntl(baseratePipe.stdout.fileno(), fcntl.F_GETFL) | os.O_NONBLOCK
+    fcntl.fcntl(baseratePipe.stdout.fileno(), fcntl.F_SETFL, mode) # file lock
+    while baseratePipe.poll() == None:
+        readx = select.select([baseratePipe.stdout.fileno()], [], [])[0]
+        if readx:
+            while True:
+                try:
+                    line = baseratePipe.stdout.readline()
+                except IOError:
+                    continue
+                break
+            throughput, loss = parse_throughput_loss_per_line(line)
+            lat, lon, speed, time = getGpsData(gps_info)
+            print >> outf, "%g %.6f %.6f %.3f %.3f %g" % (time, lat, lon, speed, throughput, loss)
+            outf.flush()
+            
+    if outputFile != None:
+        close(outf)
